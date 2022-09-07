fcbb5d00eba9f43703e00acd8d2e27e771e3aa6e
sectionr0 <sectionr0@gmail.com>
2022-09-07 21:18:13 +0900

    init

---
+++ src/main/java/com/example/test/MainController.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "hello";
+    }
+}
