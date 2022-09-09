1e0a31135af2b1bfd924473240211e7275716a75
sectionr0 <sectionr0@gmail.com>
2022-09-09 17:28:04 +0900

    properties test1

--- src/main/java/com/example/test/MainController.java
+++ src/main/java/com/example/test/MainController.java
@@ -1,13 +1,22 @@
 package com.example.test;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+    @Value("${test.abc}")
+    private String testMsg;
+
     @GetMapping("/hello")
     public String helloWorld() {
         return "test hello";
+    }
+
+    @GetMapping("/test")
+    public String testWorld() {
+        return testMsg;
     }
 }
